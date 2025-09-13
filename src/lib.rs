1b6f4b5d47bbea2c4e33ec5cda27ca14c0bb11a4
xiyosenstore <ziyosenstore@gmail.com>
2025-09-14 00:06:22 +0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -26,8 +26,8 @@ async fn main(req: Request, env: Env, _: Context) -> Result<Response> {
 
     Router::with_data(config)
         .on_async("/", fe)
-        .on_async("/free/cc/:proxyip", tunnel)
-        .on_async("/free/:proxyip", tunnel)
+        .on_async("/sub", sub)
+        .on_async("/Benxx-Project/:proxyip", tunnel)
         .on_async("/:proxyip", tunnel)
         .run(req, env)
         .await
